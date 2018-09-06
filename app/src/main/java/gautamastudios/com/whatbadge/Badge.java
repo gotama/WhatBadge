a5d45253fc62daa648690894720b2e937ebf9991
gotama <jhawkemail@gmail.com>
2018-09-06 12:28:12 +0200

    Added some functionality

--- app/src/main/java/gautamastudios/com/whatbadge/Badge.java
+++ app/src/main/java/gautamastudios/com/whatbadge/Badge.java
@@ -8,33 +8,28 @@ import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
 import android.support.annotation.ColorInt;
 import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
 import android.util.AttributeSet;
 import android.util.TypedValue;
-import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import static android.widget.RelativeLayout.ALIGN_PARENT_BOTTOM;
-import static android.widget.RelativeLayout.ALIGN_PARENT_TOP;
-import static android.widget.RelativeLayout.BELOW;
-import static android.widget.RelativeLayout.CENTER_HORIZONTAL;
-import static android.widget.RelativeLayout.TRUE;
-
-public class Badge extends LinearLayout {
+public class Badge extends RelativeLayout {
 
     private final static int OUTER_LAYOUT = 1, INNER_LAYOUT = 2, OUTER_TEXT_VIEW = 3,
-            INNER_CIRCLE = 4, INNER_TEXT_VIEW = 5, INNER_RING = 6;
+            INNER_CIRCLE = 4, INNER_TEXT_VIEW = 5, INNER_RING = 6, PARENT_LAYOUT = 7;
     private final static float innerBadgeWidth = 0.09f, innerBadgeHeight = 0.20f,
             topMarginOffset = 0.03f, marginOffset = 0.035f, innerCircleWidth = 0.32f;
+
     private int badgeWidth, badgeHeight, outerBorderColor, titleTextSize, titleTextColor,
             innerContainerColor, innerCircleColor, innerCircleBorderColor, innerTextSize,
             innerTextColor, innerCircleBorderWidth;
     private String titleText, innerText;
     private float cornerRadius;
+    private Drawable centerIcon;
 
     public Badge(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
@@ -50,11 +45,12 @@ public class Badge extends LinearLayout {
             innerContainerColor = typedArray.getColor(R.styleable.BadgeView_inner_container_background_color, Color.BLACK);
             innerCircleColor = typedArray.getColor(R.styleable.BadgeView_inner_container_circle_color, Color.BLACK);
             innerCircleBorderColor = typedArray.getColor(R.styleable.BadgeView_inner_container_circle_border_color, Color.BLACK);
-            innerCircleBorderWidth = typedArray.getDimensionPixelSize(R.styleable.BadgeView_badge_height, 6);
+            innerCircleBorderWidth = typedArray.getInteger(R.styleable.BadgeView_inner_container_circle_border_width, 6);
             innerText = typedArray.getString(R.styleable.BadgeView_inner_text);
             innerTextSize = typedArray.getDimensionPixelSize(R.styleable.BadgeView_inner_text_size, 24);
             innerTextColor = typedArray.getColor(R.styleable.BadgeView_inner_text_color, Color.BLACK);
             cornerRadius = typedArray.getFloat(R.styleable.BadgeView_corner_radius, 0f);
+            centerIcon = typedArray.getDrawable(R.styleable.BadgeView_center_icon);
         } finally {
             typedArray.recycle();
         }
@@ -62,34 +58,40 @@ public class Badge extends LinearLayout {
         init(attrs);
     }
 
+    private View outerBorderView;
+    private TextView titleTextView;
+    private View circleBorder;
+
     private void init(AttributeSet attrs) {
         //Outer Border
-        RelativeLayout parentLinearLayout = new RelativeLayout(getContext());
-        parentLinearLayout.setLayoutParams(generateParams(OUTER_LAYOUT, 0));
-        parentLinearLayout.setBackground(generateDrawable(outerBorderColor, OUTER_LAYOUT));
+        outerBorderView = new View(getContext());
+        outerBorderView.setLayoutParams(generateParams(OUTER_LAYOUT, 0));
+        outerBorderView.setBackground(generateDrawable(outerBorderColor, OUTER_LAYOUT));
 
         //Title TextView
-        TextView titleTextView = new TextView(getContext());
+        titleTextView = new TextView(getContext());
+        titleTextView.setId(View.generateViewId());
         titleTextView.setText(titleText);
         titleTextView.setTextSize(TypedValue.COMPLEX_UNIT_PX, dpToPx(titleTextSize));
         titleTextView.setLayoutParams(generateParams(OUTER_TEXT_VIEW, 0));
         titleTextView.setTypeface(Typeface.DEFAULT, Typeface.BOLD_ITALIC);
         titleTextView.setTextColor(titleTextColor);
-        parentLinearLayout.addView(titleTextView);
 
         //Inner Child Container
         RelativeLayout childLinearLayout = new RelativeLayout(getContext());
-        childLinearLayout.setLayoutParams(generateParams(INNER_LAYOUT, 0));
+        childLinearLayout.setLayoutParams(generateParams(INNER_LAYOUT, titleTextView.getId()));
         childLinearLayout.setBackground(generateDrawable(innerContainerColor, INNER_LAYOUT));
 
-        //INNER CIRCLE
+        //Inner Circle ImageView
         ImageView circleImageView = new ImageView(getContext(), attrs);
         circleImageView.setId(View.generateViewId());
         circleImageView.setLayoutParams(generateParams(INNER_CIRCLE, 0));
-        circleImageView.setImageDrawable(generateDrawable(innerCircleColor, INNER_CIRCLE));
+        circleImageView.setBackground(generateDrawable(innerCircleColor, INNER_CIRCLE));
+        circleImageView.setScaleType(ImageView.ScaleType.CENTER);
+        circleImageView.setImageDrawable(centerIcon);
 
         //Circle Border
-        View circleBorder = new View(getContext(), attrs);
+        circleBorder = new View(getContext(), attrs);
         circleBorder.setLayoutParams(generateParams(INNER_RING, 0));
         circleBorder.setBackground(generateDrawable(innerCircleBorderColor, INNER_RING));
 
@@ -105,40 +107,51 @@ public class Badge extends LinearLayout {
         childLinearLayout.addView(circleImageView);
         childLinearLayout.addView(circleBorder);
         childLinearLayout.addView(subText);
-        parentLinearLayout.addView(childLinearLayout);
 
-        this.addView(parentLinearLayout);
+        this.addView(outerBorderView);
+        this.addView(titleTextView);
+        this.addView(childLinearLayout);
+        this.setLayoutParams(generateParams(PARENT_LAYOUT, 0));
     }
 
     private ViewGroup.LayoutParams generateParams(int layoutType, int viewIdForRule) {
 
         switch (layoutType) {
+            case PARENT_LAYOUT:
+                ConstraintLayout.LayoutParams parentParms = new ConstraintLayout.LayoutParams(
+                        ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                return parentParms;
             case OUTER_LAYOUT:
-                LinearLayout.LayoutParams outerLayoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-                outerLayoutParams.gravity = Gravity.BOTTOM;
+                RelativeLayout.LayoutParams outerLayoutParams = new RelativeLayout.LayoutParams(
+                        ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
                 outerLayoutParams.height = dpToPx(badgeHeight);
                 outerLayoutParams.width = dpToPx(badgeWidth);
                 return outerLayoutParams;
             case INNER_LAYOUT:
-                RelativeLayout.LayoutParams innerLayoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-                innerLayoutParams.addRule(ALIGN_PARENT_BOTTOM, TRUE);
+                RelativeLayout.LayoutParams innerLayoutParams = new RelativeLayout.LayoutParams(
+                        ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
                 innerLayoutParams.addRule(CENTER_HORIZONTAL, TRUE);
+                innerLayoutParams.addRule(BELOW, viewIdForRule);
                 innerLayoutParams.height = dpToPx(Math.round(badgeHeight * (1 - innerBadgeHeight)));
                 innerLayoutParams.width = dpToPx(Math.round(badgeWidth * (1 - innerBadgeWidth)));
-                innerLayoutParams.setMargins(
-                        dpToPx(Math.round(badgeWidth - (badgeWidth * (1 - marginOffset)))),
-                        dpToPx(Math.round(badgeHeight - (badgeHeight * (1 - topMarginOffset)))),
-                        dpToPx(Math.round(badgeWidth - (badgeWidth * (1 - marginOffset)))),
-                        dpToPx(Math.round(badgeWidth - (badgeWidth * (1 - marginOffset)))));
+                innerLayoutParams.setMargins(0, 26, 0, 0);
+                //TODO Scale inner layout if needed
+//                innerLayoutParams.setMargins(
+//                        dpToPx(Math.round(badgeWidth - (badgeWidth * (1 - marginOffset)))),
+//                        dpToPx(Math.round(badgeHeight - (badgeHeight * (1 - topMarginOffset)))),
+//                        dpToPx(Math.round(badgeWidth - (badgeWidth * (1 - marginOffset)))),
+//                        dpToPx(Math.round(badgeWidth - (badgeWidth * (1 - marginOffset)))));
                 return innerLayoutParams;
             case OUTER_TEXT_VIEW:
-                RelativeLayout.LayoutParams outerTextViewParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                RelativeLayout.LayoutParams outerTextViewParams = new RelativeLayout.LayoutParams(
+                        ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
                 outerTextViewParams.addRule(CENTER_HORIZONTAL | ALIGN_PARENT_TOP, TRUE);
                 outerTextViewParams.topMargin = 15;
                 return outerTextViewParams;
             case INNER_CIRCLE:
             case INNER_RING:
-                RelativeLayout.LayoutParams innerCircleParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                RelativeLayout.LayoutParams innerCircleParams = new RelativeLayout.LayoutParams(
+                        ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
                 innerCircleParams.addRule(CENTER_HORIZONTAL, TRUE);
 
                 innerCircleParams.height = dpToPx(Math.round(badgeWidth * (1 - innerCircleWidth)));
@@ -150,7 +163,8 @@ public class Badge extends LinearLayout {
                         dpToPx(24));
                 return innerCircleParams;
             case INNER_TEXT_VIEW:
-                RelativeLayout.LayoutParams innerTextViewParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                RelativeLayout.LayoutParams innerTextViewParams = new RelativeLayout.LayoutParams(
+                        ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
                 innerTextViewParams.addRule(CENTER_HORIZONTAL, TRUE);
                 innerTextViewParams.addRule(BELOW, viewIdForRule);
                 innerTextViewParams.setMargins(
@@ -195,5 +209,14 @@ public class Badge extends LinearLayout {
                 .getDisplayMetrics()
                 .density;
         return Math.round((float) dp * density);
+    }
+
+    public void hideOuterBorder(boolean hide) {
+        outerBorderView.setVisibility(hide ? INVISIBLE : VISIBLE);
+        titleTextView.setVisibility(hide ? INVISIBLE : VISIBLE);
+    }
+
+    public void hideCircleBorder(boolean hide) {
+        circleBorder.setVisibility(hide ? INVISIBLE : VISIBLE);
     }
 }
--- app/src/main/java/gautamastudios/com/whatbadge/MainActivity.java
+++ app/src/main/java/gautamastudios/com/whatbadge/MainActivity.java
@@ -2,12 +2,37 @@ package gautamastudios.com.whatbadge;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Badge badge;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        badge = findViewById(R.id.badge);
+        button = findViewById(R.id.hide_button);
+        button.setTag(false);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if ((boolean) button.getTag()) {
+                    button.setTag(false);
+                    badge.hideOuterBorder(false);
+                    badge.hideCircleBorder(true);
+                } else {
+                    button.setTag(true);
+                    badge.hideOuterBorder(true);
+                    badge.hideCircleBorder(false);
+                }
+
+
+            }
+        });
     }
 }
